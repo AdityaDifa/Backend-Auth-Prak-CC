c25bd0269dde549e881d10f36be047e3b5d233f5
Aditya Difa Amanullah <adityadifa123@gmail.com>
2024-05-27 20:33:23 +0700

    secod

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ const bodyParser = require('body-parser');
 const connection = require('./db');
 
 const app = express();
-const port = 8080;
-
-app.get('/users', (req, res) => {
-    const query = 'SELECT * FROM users';
-    connection.query(query, (err, results) => {
-      if (err) {
-        console.error('Error fetching data from database:', err);
-        res.status(500).send('Internal Server Error');
-      } else {
-        res.json(results);
-      }
-    });
-  });
-  
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
+
+app.get('/users', (req, res) => {
+  const query = 'SELECT * FROM users';
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Error fetching data from database:', err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
